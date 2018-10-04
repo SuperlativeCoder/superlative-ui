caf63be4af81339a6f6a659756f35f2dac06aca5
ChangerHe <244737162@qq.com>
2018-10-04 15:00:36 +0800

    feat: add webpack and typescript

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+const UglifyjsPlugin = require('uglifyjs-webpack-plugin');
+const env = process.env
+
+const config = {
+  mode: env.NODE_ENV ? 'production' : 'development',
+  entry: path.resolve(__dirname, 'components/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }, {
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'ts-loader'
+      }
+    }]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        resolve: {
+          extensions: ['', '.ts', '.tsx']
+        }
+      }
+    })
+  ],
+  //压缩js
+  optimization: {
+    minimizer: [
+      new UglifyjsPlugin({
+        uglifyOptions: {
+          compress: false
+        }
+      })
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx'] ,
+    modules: ['src' ,'node_modules']
+  } ,
+  devtool: env.NODE_ENV ? 'source-maps' : 'eval',
+}
+
+module.exports = config
